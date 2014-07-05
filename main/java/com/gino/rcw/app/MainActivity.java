7a5f68ef572d946bce1f2516ee4cf9f89117100e
Gino <ginozhao2010@gmail.com>
2014-07-05 21:15:39 +0800

    Revise code and add globalization chinese language support

--- main/java/com/gino/rcw/app/MainActivity.java
+++ main/java/com/gino/rcw/app/MainActivity.java
@@ -1,15 +1,9 @@
 package com.gino.rcw.app;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.io.DataOutputStream;
-import java.io.IOException;
 
 //only for debug
 public class MainActivity extends ActionBarActivity {
@@ -17,13 +11,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        try {
-//            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-//            NetworkInfo info = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-//        }catch (Exception e){
-//            Log.e("mhy", e.getMessage());
-//        }
-//        isConnectWIFI(this);
     }
 
 
@@ -44,60 +31,5 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public boolean start(){
-        Runtime runtime = Runtime.getRuntime();
-        Process pro;
-        try {
-            pro = runtime.exec("su");
-            DataOutputStream dos = new DataOutputStream(pro.getOutputStream());
-            dos.writeBytes("setprop service.adb.tcp.port 5555\n");
-            dos.writeBytes("stop adbd\n");
-            dos.writeBytes("start adbd\n");
-            dos.flush();
-            dos.close();
-            if(pro.waitFor() == 0){
-                return true;
-            }else{
-                return false;
-            }
-        } catch (IOException e) {
-            return false;
-        } catch (InterruptedException e) {
-            return false;
-        }
-    }
-
-    public boolean stop(){
-        Runtime runtime = Runtime.getRuntime();
-        Process pro;
-        try {
-            pro = runtime.exec("su");
-            DataOutputStream dos = new DataOutputStream(pro.getOutputStream());
-            dos.writeBytes("setprop service.adb.tcp.port -1\n");
-            dos.writeBytes("stop adbd\n");
-            dos.writeBytes("start adbd\n");
-            dos.flush();
-            dos.close();
-            if(pro.waitFor() == 0){
-                return true;
-            }else{
-                return false;
-            }
-        } catch (IOException e) {
-            return false;
-        } catch (InterruptedException e) {
-            return false;
-        }
-    }
-
-    public boolean isConnectWIFI(Context context) {
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo info = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (info != null) {
-            return info.isConnected();
-        }
-        return false;
     }
 }
--- main/java/com/gino/rcw/app/RemoteConnect.java
+++ main/java/com/gino/rcw/app/RemoteConnect.java
@@ -6,12 +6,10 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
@@ -19,7 +17,6 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Properties;
 
 /**
  * Implementation of App Widget functionality.
@@ -149,7 +146,7 @@ public class RemoteConnect extends AppWidgetProvider {
         WifiManager wifiManger = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         WifiInfo wifiInfo = wifiManger.getConnectionInfo();
         int ip = wifiInfo.getIpAddress();
-        return " ip:" + (ip & 0xFF) + "." + ((ip >> 8) & 0xFF) + "." + ((ip >> 16) & 0xFF) + "." + (ip >> 24 & 0xFF);
+        return " IP:" + (ip & 0xFF) + "." + ((ip >> 8) & 0xFF) + "." + ((ip >> 16) & 0xFF) + "." + (ip >> 24 & 0xFF);
     }
 }
 
